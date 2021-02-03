1911d8325c120acc22ac40adb25d9d00dfec8213
yitiaoyu928 <1915144429@qq.com>
2021-02-03 15:40:08 +0800

    添加基本样式

--- src/components/qgy-popover/popover.vue
+++ src/components/qgy-popover/popover.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="qgy-popover" @click.stop="onClick($event)">
     <div ref="contentWrapper" class="popover-wrapper" v-if="visible" @click.stop>
-      <slot name="content">这是popover的内容部分</slot>
+      <slot name="content">{{text}}</slot>
     </div>
     <span ref="triggerWrapper">
       <slot></slot>
@@ -12,6 +12,12 @@
 <script>
 export default {
   name: "popover",
+  props:{
+    text:{
+      type:String,
+      default:'hello Seven fish'
+    }
+  },
   data() {
     return {
       visible: false
@@ -19,21 +25,21 @@ export default {
   },
   methods: {
     onClick(event) {
-      if (this.$refs.triggerWrapper.contains(event.target)) {
-        this.visible = !this.visible;
-        if (this.visible === true) {
-          setTimeout(() => {
-            this.setElementPosition();
-            this.elementEvent();
-          })
-        }
+      this.visible = !this.visible;
+      if (this.visible === true) {
+        setTimeout(() => {
+          this.setElementPosition();
+          this.elementEvent();
+        })
       }
+
     },
     setElementPosition() {
       document.body.appendChild(this.$refs.contentWrapper);
       let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
-      this.$refs.contentWrapper.style.left = left + 'px';
-      this.$refs.contentWrapper.style.top = top + 'px';
+      console.log(left ,window.scrollX)
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
     },
     elementEvent() {
       let eventHandle = () => {
@@ -54,10 +60,34 @@ export default {
 
 .popover-wrapper {
   position: absolute;
+  border: 1px solid #999;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  padding: 0.5em 1em;
+  margin-top: -10px;
+  background-color: #fff;
   -webkit-transform: translateY(-100%);
   -moz-transform: translateY(-100%);
   -ms-transform: translateY(-100%);
   -o-transform: translateY(-100%);
   transform: translateY(-100%);
+
+  &::before,&::after {
+    content: '';
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 10px;
+    border: 10px solid transparent;
+  }
+  &::before {
+    top: calc(100% + 1px);
+    border-top: 10px solid #999;
+  }
+  &::after {
+    top: 100%;
+    border-top: 10px solid #fff;
+  }
 }
 </style>
