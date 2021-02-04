63d25ab5eae58cc08d9240e0e7847fb92b4b3c1a
yitiaoyu928 <1915144429@qq.com>
2021-02-04 12:41:37 +0800

    添加accordion组件

---
+++ src/components/qgy-accordion/accordion.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="qgy-accordion">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+name: "accordion"
+}
+</script>
+
+<style lang="scss" scoped>
+.qgy-accordion {
+
+}
+</style>
